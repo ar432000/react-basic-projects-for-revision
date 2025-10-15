d388e6e546a780e5eff341ebfdd35a31ed14b4c0
Abhay Raj <rajabhay432000@gmail.com>
2025-10-16 03:22:12 +0530

    completed initial setup and made few code changes

---
+++ small-projects/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Accordian from "./components/Accordian";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* Accordian component */}
+        <Accordian />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ small-projects/src/components/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: "1",
+    question: "what is react?",
+    answer: "React is a JavaScript library for building user interfaces.",
+  },
+  {
+    id: "2",
+    question: "what is jsx?",
+    answer:
+      "JSX stands for JavaScript XML. It allows us to write HTML in React.",
+  },
+  {
+    id: "3",
+    question: "what is component?",
+    answer:
+      "Components are the building blocks of a React application's UI. These components split up the entire UI into small independent and reusable pieces.",
+  },
+  {
+    id: "4",
+    question: "what is state?",
+    answer:
+      "State is a built-in React object that is used to contain data or information about the component.",
+  },
+];
+
+export default data;
